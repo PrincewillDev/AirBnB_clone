13adb2c8ba83937fe927223646b6dac273565daa
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-04-18 16:11:21 +0100

    fix: fixed a testing error in the save method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,7 +16,7 @@ class  TestBaseModel(unittest.TestCase):
     
     def test_save_method(self):
         """Check if to updated_at is greater than created_at after calling the save method: This ensures that when an item is saved the time is updated """
-        # self.assertEqual(self.model1.created_at, self.model1.updated_at)
+        self.assertEqual(self.model1.created_at, self.model1.updated_at)
         self.model1.name = "Model"
         self.model1.save()
 
