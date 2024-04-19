ca28956a4de37c5561f0b56d37001b5af272d7c9
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-04-19 17:47:28 +0100

    Add more test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,51 +1,106 @@
-""" Unittest for the base models class
+#!/usr/bin/python3
+"""Unit test for BaseModel class."""
 
-    Class: TestBaseModel
 
-"""
-
-#import unittest module
 import unittest
-
-# import module to test
 from models.base_model import BaseModel
+from datetime import datetime
 
-class  TestBaseModel(unittest.TestCase):
+
+class TestBaseModelClass(unittest.TestCase):
+    """Tests the BaseModel class of the models module class"""
+
     def setUp(self):
+        """Sets up BaseModel object for testing"""
         self.model1 = BaseModel()
-    
-    def test_save_method(self):
-        """Check if to updated_at is greater than created_at after calling the save method: This ensures that when an item is saved the time is updated """
-        self.assertEqual(self.model1.created_at, self.model1.updated_at)
-        self.model1.name = "Model"
-        self.model1.save()
+        self.model2 = BaseModel()
 
+    def tearDown(self):
+        """Dispose test object"""
+        del self.model1
+        del self.model2
+
+    def test_constructor(self):
+        """Tests the class constructor"""
+        self.assertIsInstance(self.model1, BaseModel)
+        self.assertIsInstance(self.model2, BaseModel)
+
+        # Test that object re-creation from its dict representation works:
+
+        # Generate dictionary representation of original object (self.model1)
+        actual_obj_dict = self.model1.to_dict()
+
+        # Re-create original object from its dictionary representation
+        recreated_obj = BaseModel(**actual_obj_dict)
+
+        # Check that str represantations of actual obj and re-cereated are same
+        self.assertEqual(str(recreated_obj), str(self.model1))
+
+    def test_attr_id(self):
+        """Tests the id attribute of instance"""
+        self.assertTrue(hasattr(self.model1, 'id'))
+        self.assertIsInstance(self.model1.id, str)
+        self.assertNotEqual(self.model1.id, self.model2.id)
+
+    def test_attr_created_at(self):
+        """Tests the created_at attribute of instance"""
+        self.assertTrue(hasattr(self.model1, 'created_at'))
+        self.assertIsInstance(self.model1.created_at, datetime)
+
+    def test_attr_updated_at(self):
+        """Tests the updated_at attribute of instance"""
+        self.assertTrue(hasattr(self.model1, 'updated_at'))
+        self.assertIsInstance(self.model1.updated_at, datetime)
+
+    def test_method_str(self):
+        """Tests the str method"""
+        expected_str = f"[BaseModel] ({self.model1.id}) {self.model1.__dict__}"
+        actual_str = str(self.model1)
+        self.assertMultiLineEqual(expected_str, actual_str)
+
+    def test_method_save(self):
+        """Tests the save public instance method"""
+
+        # Check that created_at == updated_at when instance is created
+        self.assertEqual(self.model1.created_at, self.model1.updated_at)
+
+        # Check that created_at != updated_at after instance is updated
+        self.model1.name = "Model 1"
+        self.model1.save()
         self.assertNotEqual(self.model1.created_at, self.model1.updated_at)
+
+        # Check that updated_at > created_at after instance is updated
         self.assertGreaterEqual(self.model1.updated_at, self.model1.created_at)
 
-    def test_to_dict_method(self):
-        """Testing the to_dict method: To ensure it returns data as dictionary"""
-        baseModelDict = self.model1.to_dict()
+    def test_method_to_dict(self):
+        """Tests the to_dict public instance method"""
+        expected_dict = self.model1.to_dict()
+
+        # Check that only instance attributes set are in expected_dict
+        self.assertNotIn('unset_attr', expected_dict)
+
+        # Check that __class__ key and value exists
+        self.assertIn('__class__', expected_dict)
+
         # Check that value of __class__ key is class name of object
-        self.assertEqual(baseModelDict['__class__'], 'BaseModel')
-        self.assertIsInstance(baseModelDict, dict)      
+        self.assertEqual(expected_dict['__class__'], 'BaseModel')
 
-    def test_id_attribute(self):
-        """Testing if the right uuid is generated"""
-        baseModelId = self.model1.id
-        self.assertTrue(baseModelId)
+        # Check that created_at and updated_at attr of dict are string objects
+        self.assertIsInstance(expected_dict['created_at'], str)
+        self.assertIsInstance(expected_dict['updated_at'], str)
 
-    def test_created_at_attribute(self):
-        """Testing if the right time is generated when an instance is created"""
+        # Check that created_at and updated_at attr of dict are in ISO format
+        def is_ISO_format(datetime_str):
+            """Attempt to parse a string into datetime object in ISO format"""
+            try:
+                datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%f")
+                return True
+            except ValueError:
+                return False
 
-        baseModelInstance = self.model1.created_at
-        self.assertTrue(baseModelInstance)
-    
-    def test_str_method(self):
-        """Testing the str method to ensure that instance of the class is returned correctly in str format"""
+        self.assertTrue(is_ISO_format(expected_dict['created_at']))
+        self.assertTrue(is_ISO_format(expected_dict['updated_at']))
 
-        baseModelInstance_str = f"[BaseModel] ({self.model1.id}) {self.model1.__dict__}"
-        basemodel_str = str(self.model1)
-        self.assertMultiLineEqual(baseModelInstance_str, basemodel_str)
+
 if __name__ == "__main__":
     unittest.main()
