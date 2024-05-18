d4f856780feff344578f2dc472092bb5e850cdb5
PrincewillDev <princewillfidelis1@gmail.com>
2024-05-18 15:33:29 +0100

    Fix: fixed syntax error in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,28 @@
 """
 This module defines the base model for the AirBnB data table.
 """
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 import models
+
+
 class BaseModel:
     """
-        This class defines the BaseModel class which is the parent class
-        for all child class that will be created for the AirBnB data set.
+    This class defines the BaseModel class which is the parent class
+    for all child class that will be created for the AirBnB data set.
 
-        Attributes:
-            id: This attribute is responsible for creating a unique id
-            when an instance is created.
-            
-            created_at: This attribute is responsible for getting the
-            current date and time an instance is created.
+    Attributes:
+        id: This attribute is responsible for creating a unique id
+        when an instance is created.
 
-            updated_at: This attribute is responsible for getting the
-            current date and time an instance is created.
+        created_at: This attribute is responsible for getting the
+        current date and time an instance is created.
+
+        updated_at: This attribute is responsible for getting the
+        current date and time an instance is created.
 
     """
+
     def __init__(self, *args, **kwargs):
         """
         The initialization method
@@ -28,19 +31,19 @@ class BaseModel:
 
         if kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f")
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
 
                 if key != "__class__":
                     try:
                         setattr(self, key, value)
-                    except:
+                    except (ValueError, TypeError):
                         raise ValueError
 
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -48,15 +51,15 @@ class BaseModel:
         This method return/prints the class details in a string format.
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """
         This method updates the public instance attribute
         updated_at with the current datetime.
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
-    
+
     def to_dict(self):
         """
         This method returns a dictionary containing all
