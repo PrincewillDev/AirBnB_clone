bbd70173c4c185dc3e9d3b1a98cc814d0679e524
PrincewillDev <princewillfidelis1@gmail.com>
2024-05-20 16:09:25 +0100

    Test: testing the save method that serializes data to a json file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,7 +6,7 @@ import os
 from models.engine.file_storage import FileStorage
 from models.user import User
 from models.base_model import BaseModel
-import models
+import json
 class TestFileStorage(unittest.TestCase):
     # Test class atrribute: __file_path, __objects.
     # Test all method
@@ -17,7 +17,6 @@ class TestFileStorage(unittest.TestCase):
         self.file_storage = FileStorage()
         
         
-    
     def test_file_path(self):
         self.assertTrue(os.path.exists(self.file_storage._FileStorage__file_path))
         
@@ -54,4 +53,17 @@ class TestFileStorage(unittest.TestCase):
         self.assertIn(obj_key, self.file_storage.all())
         self.assertIsInstance(__object_dict, dict)
         
+    def test_save_method(self):
+        self.new_Object = BaseModel()
         
+        newObj_dict = {}
+        obj_key = f"{self.__class__.__name__}.{str(self.new_Object.id)}"
+        obj_value = self.new_Object.to_dict()
+        newObj_dict[obj_key] = obj_value
+        
+        with open(self.file_storage._FileStorage__file_path, 'w', encoding='utf-8') as jsonfile:
+            json.dump(newObj_dict, jsonfile)
+
+        with open(self.file_storage._FileStorage__file_path, 'r') as jsonfile:
+            jsonfile_obj = jsonfile.read()
+            self.assertIn(json.dumps(newObj_dict), jsonfile_obj)
