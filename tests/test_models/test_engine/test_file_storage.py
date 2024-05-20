ec6eec896d99839578cdc1c0c9acccfb64e16457
PrincewillDev <princewillfidelis1@gmail.com>
2024-05-21 00:31:10 +0100

    Fix: Fix error with testcase in reload()

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,155 +1,89 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+"""This module is uses unittest to test the file storage model / storage engine.
 """
-Created on Fri Jun  5 15:43:09 2020
-@author: meco
-"""
-import sys
 import unittest
-import inspect
-import io
-import pycodestyle
-from datetime import datetime
-from contextlib import redirect_stdout
-from models.base_model import BaseModel
+import os
 from models.engine.file_storage import FileStorage
-from models import storage
 from models.user import User
-
-
+from models.base_model import BaseModel
+import json
+from models.place import Place
+from datetime import datetime
 class TestFileStorage(unittest.TestCase):
-    """
-    class for testing FileStorage class' methods
-    """
-    temp_file = ""
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        Set up class method for the doc tests
-        """
-        cls.setup = inspect.getmembers(FileStorage, inspect.isfunction)
-
-    def test_pep8_conformance_FileStorage(self):
-        """
-        Test that file_storage.py file conform to PEP8
-        """
-        pep8style = pycodestyle.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/file_storage.py'])
-        self.assertEqual(result.total_errors, 1,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_FileStorage(self):
-        """
-        Test that test_file_storage.py file conform to PEP8
-        """
-        pep8style = pycodestyle.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/\
-                                        test_file_storage.py'])
-        self.assertEqual(result.total_errors, 1,
-                         "Found code style errors (and warnings).")
-
-    def test_module_docstring(self):
-        """
-        Tests if module docstring documentation exist
-        """
-        self.assertTrue(len(FileStorage.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """
-        Tests if class docstring documentation exist
-        """
-        self.assertTrue(len(FileStorage.__doc__) >= 1)
-
-    def test_func_docstrings(self):
-        """
-        Tests if methods docstring documntation exist
-        """
-        for func in self.setup:
-            self.assertTrue(len(func[1].__doc__) >= 1)
-
-    @staticmethod
-    def move_file(src, dest):
-        with open(src, 'r', encoding='utf-8') as myFile:
-            with open(dest, 'w', encoding='utf-8') as tempFile:
-                tempFile.write(myFile.read())
-        os.remove(src)
-
+    # Test class atrribute: __file_path, __objects.
+    # Test all method
+    # Test new method
+    # Test save method
+    # Test reload method
     def setUp(self):
-        self.temp_file = '/temp_store.json'
-        self.temp_objs = [BaseModel(), BaseModel(), BaseModel()]
-        for obj in self.temp_objs:
-            storage.new(obj)
-        storage.save()
-
-    def tearDown(self):
-        """initialized object
-        """
-        del self.temp_objs
-
-    def test_type(self):
-        """type checks for FileStorage
-        """
-        self.assertIsInstance(storage, FileStorage)
-        self.assertEqual(type(storage), FileStorage)
-
+        self.file_storage = FileStorage()
+        # self.file_path = "file.json"
+        
+    def test_file_path(self):
+        self.assertTrue(os.path.exists(self.file_storage._FileStorage__file_path))
+        
+    def test_objects(self):
+        self.assertIsInstance(self.file_storage._FileStorage__objects, dict)
+        
+    def test_all_method(self):
+        allObjects = self.file_storage.all()
+        self.assertIsInstance(allObjects, dict)
+        Objectlength = len(allObjects)
+        
+        # Testing that all() updates with  new object
+        self.newObject = User()
+        self.assertIn(self.newObject, allObjects.values())
+        self.assertTrue(len(allObjects) == Objectlength + 1)
+        
+       
+        self.newObject2 = User()
+        self.assertIn(self.newObject2, allObjects.values())
+        self.assertTrue(len(allObjects) == Objectlength + 2)
+        
+        # Testing if the object returned by all() is same as the value in __objects 
+        self.assertEqual(self.file_storage.all(), self.file_storage._FileStorage__objects)
+    
+    def test_new_method(self):
+        self.newObj = BaseModel()
+        self.file_storage.new(self.newObj)
+        
+        __object_dict = {}
+        objValue = self.newObj.to_dict()
+        obj_key = '{}.{}'.format(self.newObj.__class__.__name__, str(self.newObj.id))
+        __object_dict[obj_key] = objValue
+        
+        self.assertIn(obj_key, self.file_storage.all())
+        self.assertIsInstance(__object_dict, dict)
+        
+    def test_save_method(self):
+        self.new_Object = BaseModel()
+        self.file_storage.save()
+        self.obj_key = f"BaseModel.{str(self.new_Object.id)}"
+        
+        with open(self.file_storage._FileStorage__file_path, 'r') as jsonfile:
+            jsonfile_content = jsonfile.read()
+        
+        self.assertIn(self.obj_key, jsonfile_content)
+        
     def test_save(self):
-        """tests save functionality for FileStorage
-        """
-        self.filestorage = FileStorage()
-        with open(self.filestorage._FileStorage__file_path, 'r', encoding='utf-8') as myFile:
-            dump = myFile.read()
-        self.assertNotEqual(len(dump), 0)
-        temp_d = eval(dump)
-        key = self.temp_objs[0].__class__.__name__ + '.'
-        key += str(self.temp_objs[0].id)
-        self.assertNotEqual(len(temp_d[key]), 0)
-        key2 = 'State.412409120491902491209491024'
-        try:
-            self.assertRaises(temp_d[key2], KeyError)
-        except:
-            pass
-
-    def test_reload(self):
-        """tests reload functionality for FileStorage
-        """
-        storage.reload()
-        obj_d = storage.all()
-        key = self.temp_objs[1].__class__.__name__ + '.'
-        key += str(self.temp_objs[1].id)
-        self.assertNotEqual(obj_d[key], None)
-        self.assertEqual(obj_d[key].id, self.temp_objs[1].id)
-        key2 = 'State.412409120491902491209491024'
-        try:
-            self.assertRaises(obj_d[key2], KeyError)
-        except:
-            pass
-
-    def test_delete_basic(self):
-        """tests delete basic functionality for FileStorage
-        """
-        obj_d = storage.all()
-        key2 = self.temp_objs[2].__class__.__name__ + '.'
-        key2 += str(self.temp_objs[2].id)
-        try:
-            self.assertRaises(obj_d[key2], KeyError)
-        except:
-            pass
-
-    def test_new_basic(self):
-        """tests new basic functionality for FileStorage
-        """
-        obj = BaseModel()
-        storage.new(obj)
-        obj_d = storage.all()
-        key = obj.__class__.__name__ + '.' + str(obj.id)
-        self.assertEqual(obj_d[key] is obj, True)
-
-    def test_new_badinput(self):
-        """tests new bad input functionality for FileStorage
-        """
-        try:
-            self.assertRaises(storage.new('jwljfef'), TypeError)
-            self.assertRaises(storage.new(None), TypeError)
-        except:
-            pass
+        self.new_obj = BaseModel()
+        self.new_obj.save()
+        
+        self.assertIsInstance(self.new_obj.created_at, datetime)
+        
+    def test_reload_method(self):
+        self.place = Place()
+        self.file_storage.save()
+        self.file_storage.reload()
+        
+        allObj = self.file_storage.all()
+        # obj_key = f"Place.{self.place.id}"
+        
+        for key in allObj.keys():
+            if key == f"Place.{self.place.id}":
+                reloaded_obj = allObj[key]
+        
+        self.assertEqual(self.place.id, reloaded_obj.id)
+        self.assertEqual(self.place.created_at, reloaded_obj.created_at)
+        self.assertEqual(self.place.updated_at, reloaded_obj.updated_at)
+        self.assertTrue(reloaded_obj, allObj)
