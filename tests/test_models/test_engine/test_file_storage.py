88a50976d4dd417371cb09ecc47358ee4468f253
PrincewillDev <princewillfidelis1@gmail.com>
2024-05-20 03:07:15 +0100

    Test: wrote test for the all and new methods with unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,7 +4,9 @@
 import unittest
 import os
 from models.engine.file_storage import FileStorage
-
+from models.user import User
+from models.base_model import BaseModel
+import models
 class TestFileStorage(unittest.TestCase):
     # Test class atrribute: __file_path, __objects.
     # Test all method
@@ -13,9 +15,43 @@ class TestFileStorage(unittest.TestCase):
     # Test reload method
     def setUp(self):
         self.file_storage = FileStorage()
+        
+        
     
     def test_file_path(self):
         self.assertTrue(os.path.exists(self.file_storage._FileStorage__file_path))
         
     def test_objects(self):
         self.assertIsInstance(self.file_storage._FileStorage__objects, dict)
+        
+    def test_all_method(self):
+        allObjects = self.file_storage.all()
+        self.assertIsInstance(allObjects, dict)
+        Objectlength = len(allObjects)
+        
+        # Testing that all() updates with  new object
+        self.newObject = User()
+        self.assertIn(self.newObject, allObjects.values())
+        self.assertTrue(len(allObjects) == Objectlength + 1)
+        
+       
+        self.newObject2 = User()
+        self.assertIn(self.newObject2, allObjects.values())
+        self.assertTrue(len(allObjects) == Objectlength + 2)
+        
+        # Testing if the object returned by all() is same as the value in __objects 
+        self.assertEqual(self.file_storage.all(), self.file_storage._FileStorage__objects)
+    
+    def test_new_method(self):
+        self.newObj = BaseModel()
+        self.file_storage.new(self.newObj)
+        
+        __object_dict = {}
+        objValue = self.newObj.to_dict()
+        obj_key = '{}.{}'.format(self.newObj.__class__.__name__, str(self.newObj.id))
+        __object_dict[obj_key] = objValue
+        
+        self.assertIn(obj_key, self.file_storage.all())
+        self.assertIsInstance(__object_dict, dict)
+        
+        
